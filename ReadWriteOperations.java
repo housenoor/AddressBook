dd7c70e1502f7a23ec66f6d4678e842f8fb19b18
Srikanth <srikanthh994@gmail.com>
2021-11-12 07:36:14 +0530

    Added:To Read and Write using jsonFile

--- ReadWriteOperations.java
+++ ReadWriteOperations.java
@@ -8,38 +8,27 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class ReadWriteOperations {
-    String filePath = "C:\\Users\\admin\\IdeaProjects\\AddressBookUsingLib\\src\\AddressBook.txt";
+	String filePath = "C:\\Users\\admin\\IdeaProjects\\AddressBookUsingLib\\src\\AddressBook.txt";
 
-    /**
-     * Purpose : Ability to Write the Address Book with Persons Contact into a File using File IO
-     *
-     * @param : personInfoDict
-     */
-    public void writeInAddressBook(Hashtable<String, ArrayList<contactInfo>> personInfoDict) {
-        StringBuffer empBuffer = new StringBuffer();
-        personInfoDict.forEach((companyName, personInfos) -> {
-            String empDataString = companyName.concat(personInfos.toString().concat("\n"));
-            empBuffer.append(empDataString);
-        });
+	public void writeInAddressBook(Hashtable<String, ArrayList<contactInfo>> personInfoDict) {
+		StringBuffer empBuffer = new StringBuffer();
+		personInfoDict.forEach((companyName, personInfos) -> {
+			String empDataString = companyName.concat(personInfos.toString().concat("\n"));
+			empBuffer.append(empDataString);
+		});
 
-        try {
-            Files.write(Paths.get(filePath), empBuffer.toString().getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			Files.write(Paths.get(filePath), empBuffer.toString().getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Purpose : Ability to Read the Address Book with Persons Contact into a File using File IO
-     *
-     * @param : personInfoDict
-     */
-    public void readFromAddressBook() {
-        try {
-            Files.lines(new File(filePath).toPath())
-                    .forEach(System.out::println);
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-    }
+	public void readFromAddressBook() {
+		try {
+			Files.lines(new File(filePath).toPath()).forEach(System.out::println);
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
 }
